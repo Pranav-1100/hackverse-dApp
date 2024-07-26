a03ac1d8d2a269004f98a9c097d8eda3d50586b7
Aditya Singh <aditya.23bcs10148@sst.scaler.com>
2024-07-26 14:30:42 +0530

    constants added

---
+++ frontend/hv_dapp/lib/constants/custom_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hv_dapp/constants/constants.dart';
+
+// ignore: must_be_immutable
+class CustomContainer extends StatelessWidget {
+   CustomContainer({super.key, required this.containerContent,this.color,this.height,this.borderRadius,this.oppositeBorderRadius});
+  Widget containerContent;
+  Color ? color;
+  double ? height;
+  BorderRadius ? borderRadius;
+  BorderRadius? oppositeBorderRadius;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height??MediaQuery.of(context).size.height * 0.75,
+      width:width,
+      child: ClipRRect(
+        borderRadius: borderRadius??BorderRadius.only(
+          bottomLeft: Radius.circular(30.r),
+          bottomRight: Radius.circular(30.r),
+        ),
+        child: Container(
+          width: width,
+          color: color??kOffWhite,
+          child: SingleChildScrollView(
+            child:containerContent,
+          ),
+        ),
+      ),
+    );
+  }
+}
