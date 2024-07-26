dc18494a458f35eb52a1ac422ca8effdd47f7d09
Pranav <pranavaggarwal1100@gmail.com>
2024-07-26 22:32:27 +0530

    Auth added

---
+++ backend/services/authService.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const BASE_URL = "https://sandbox-api.okto.tech";
+const OKTO_CLIENT_API = "f7aa7c2d-6eab-4ca8-b441-4db577c50c17";
+
+const apiService = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "x-api-key": OKTO_CLIENT_API,
+    "Content-Type": "application/json",
+  },
+});
+
+const authenticate = async (idToken) => {
+  try {
+    const response = await apiService.post("/api/v1/authenticate", { id_token: idToken });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.error);
+  }
+};
+
+const setPin = async (idToken, token, reloginPin) => {
+  try {
+    const response = await apiService.post("/api/v1/set_pin", {
+      id_token: idToken,
+      token: token,
+      relogin_pin: reloginPin,
+      purpose: "set_pin",
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.error);
+  }
+};
+
+module.exports = { authenticate, setPin };
